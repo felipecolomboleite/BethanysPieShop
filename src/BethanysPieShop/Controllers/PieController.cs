b4df06aac9d14a538439bcfde58d1d138f8761b7
Felipe Colombo Leite <felipe.leite@disroot.org>
2021-01-10 20:17:28 -0300

    Criação da controller das pies

---
+++ src/BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+        private readonly ICategoryRepository categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            this.pieRepository = pieRepository;
+            this.categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(pieRepository.AllPies);
+        }
+    }
+}
