1aa966b81478ba995568281464451340a29292e6
Felipe Colombo Leite <felipe.leite@disroot.org>
2021-01-26 19:58:15 -0300

    Adição de página de detalhes e mudança do _layout com botão de navegação para a lista

--- src/BethanysPieShop/Controllers/PieController.cs
+++ src/BethanysPieShop/Controllers/PieController.cs
@@ -25,5 +25,17 @@ namespace BethanysPieShop.Controllers
 
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = pieRepository.GetPieById(id);
+
+            if (pie == null)
+            {
+                return NotFound();
+            }
+
+            return View(pie);
+        }
     }
 }
--- src/BethanysPieShop/Startup.cs
+++ src/BethanysPieShop/Startup.cs
@@ -51,7 +51,7 @@ namespace BethanysPieShop
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                    pattern: "{controller=Home}/{action=Index}/{id:int?}"
                 );
             });
         }
